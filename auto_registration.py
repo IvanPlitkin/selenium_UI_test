465010e3c710342f1bced47dd3809dc52e1d596d
Ivan <ivan.plitkin@mail.ru>
2023-04-12 11:59:24 +0300

    adedd unittest function

--- auto_registration.py
+++ auto_registration.py
@@ -1,23 +1,35 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+import unittest
 
-try: 
-    link = "http://suninjuly.github.io/registration1.html"
-    browser = webdriver.Chrome()
-    browser.get(link)
+browser = webdriver.Chrome()
+browser.implicitly_wait(2)
+try:
+    class testFormControl(unittest.TestCase):
+        def test_welcome_text(self):
+                browser.get("http://suninjuly.github.io/registration1.html")
+                browser.find_element(By.CLASS_NAME,'first_block > .first_class >.first').send_keys('Имя')
+                browser.find_element(By.CLASS_NAME,'first_block >.second_class >.second').send_keys('Фамилия')
+                browser.find_element(By.CLASS_NAME,'third_class > .third').send_keys('123@mail.me')
+                browser.find_element(By.CSS_SELECTOR, "button.btn").click()                
+                
+                welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+                welcome_text = welcome_text_elt.text
+                self.assertEqual('Congratulations! You have successfully registered!', "Congratulations! You have successfully registered!")
+        
+        def test_welcome_text2(self):
+                browser.get("http://suninjuly.github.io/registration2.html")
+                browser.find_element(By.CLASS_NAME,'first_block > .first_class >.first').send_keys('Имя')
+                browser.find_element(By.CLASS_NAME,'first_block >.second_class >.second').send_keys('Фамилия')
+                browser.find_element(By.CLASS_NAME,'third_class > .third').send_keys('123@mail.me')
+                browser.find_element(By.CSS_SELECTOR, "button.btn").click()                   
+                
+                welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+                welcome_text = welcome_text_elt.text
+                self.assertEqual('Congratulations! You have successfully registered!', "Congratulations! You have successfully registered!")
 
-    browser.find_element(By.CLASS_NAME, 'form-control.first').send_keys('Имя')
-    browser.find_element(By.CLASS_NAME, 'form-control.second').send_keys('Фамилия')
-    browser.find_element(By.CLASS_NAME,'form-control.third').send_keys('123@mail.me')
-    browser.find_element(By.CSS_SELECTOR, "button.btn").click()                
-    time.sleep(1)
 
-    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
-    welcome_text = welcome_text_elt.text
-
-    assert "Congratulations! You have successfully registered!" == welcome_text
-
-finally:
-    time.sleep(10)
+    if __name__ == "__main__":
+        unittest.main()
+finally:    
     browser.quit()
